51eb1a8f817e40e127766a278d04731a35230060
ahmadhamzh <ahmadhamzh@ymail.com>
2021-11-16 18:52:35 +0200

    lab 13

--- lab12-socket-io/driver/driver.js
+++ lab12-socket-io/driver/driver.js
@@ -1,24 +1,27 @@
 'use struct'
 
-
 const PORT = process.env.PORT || 3000;
+
 const io = require('socket.io-client');
 const host = 'http://localhost:3000';
 const capsConnection = io.connect(host);
 
+capsConnection.on('getAllPickup')
+capsConnection.on('getAllTransit')
 
-capsConnection.on('pickup', (payLoad)=>{
-setTimeout(()=>{
-    console.log(`Driver : picked up ${payLoad.orderId}`);
-   
-    capsConnection.emit('transit', payLoad );
 
-},2000)
+capsConnection.on('pickup', (payLoad) => {
+    capsConnection.emit('receivedPickup', payLoad);
+    setTimeout(() => {
+        console.log(`Driver : picked up ${payLoad.orderId}`);
+        capsConnection.emit('transit', payLoad);
+    }, 2000)
 })
 
-capsConnection.on('transit',(payLoad)=>{
-    
-
+capsConnection.on('transit', (payLoad) => {
+    capsConnection.emit('receivedTransit', payLoad);
+    setTimeout(() => {
         console.log(`Driver :  delivered up ${payLoad.orderId}`);
-    
+        capsConnection.emit('delivered', payLoad);
+    }, 2000)
 })
--- lab12-socket-io/fire-even/fire-event.js
+++ lab12-socket-io/fire-even/fire-event.js
@@ -1,24 +1,23 @@
 'use struct'
 
 
-const faker = require('faker')
-const io = require('socket.io-client');
-const host = 'http://localhost:3000';
-const capsConnection = io.connect(host);
+// const faker = require('faker')
+// const io = require('socket.io-client');
+// const host = 'http://localhost:3000';
+// const capsConnection = io.connect(host);
 
-
-setInterval(() => {
-    let order = 
-         {
-            store : faker.random.word(),
-            orderId : Math.ceil(Math.random()*10000),
-            custumer : faker.name.findName(),
-            address : faker.address.streetAddress()
-    } ;    
-    console.log('----------------------');    
-    // console.log(order);    
-    console.log('----------------------');    
-    capsConnection.emit('pickup', order );
-    console.log('----------------------');
+// setInterval(() => {
+//     let order = 
+//          {
+//             store : faker.random.word(),
+//             orderId : Math.ceil(Math.random()*10000),
+//             custumer : faker.name.findName(),
+//             address : faker.address.streetAddress()
+//     } ;    
+//     console.log('----------------------');    
+//     // console.log(order);    
+//     console.log('----------------------');    
+//     capsConnection.emit('pickup', order );
+//     console.log('----------------------');
   
-  }, 4000);
+//   }, 4000);
